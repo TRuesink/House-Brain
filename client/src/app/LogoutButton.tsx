fd85a53fa9f6891df9de010e24ec832b13cb083b
TRuesink <timothy.ruesink@gmail.com>
2024-04-22 12:38:15 -0600

    use mui theme

--- client/src/app/LogoutButton.tsx
+++ client/src/app/LogoutButton.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { logout } from "@/api/auth";
+import { Button } from "@mui/material";
 import React from "react";
 
 const LogoutButton = () => {
   const handleClick = async () => {
     await logout();
   };
-  return <button onClick={handleClick}>Logout</button>;
+  return <Button onClick={handleClick}>Logout</Button>;
 };
 
 export default LogoutButton;
